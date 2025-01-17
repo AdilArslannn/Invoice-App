34814c7a7b47b0f13803fd4dc183a2a0385ff267
AdilArslannn <adil.arslan@nordsec.com>
2025-01-17 10:45:58 +0000

    add button

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@
   <RouterView />
 
   <link v-bind:href="isDarkTheme ? '/styles/dark.css' : '/styles/light.css'" rel="stylesheet" />
+  <button class="purple" @click="router.push('/addresses')">Test</button>
 </template>
 
 <script setup lang="ts">
